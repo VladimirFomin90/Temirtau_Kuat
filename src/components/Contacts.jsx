c809f09f707039ba36576bfd727998d159a70ff0
VladimirFomin90 <vladimirfomin90@yandex.ru>
2022-12-19 20:54:17 +0600

    fix map

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,8 @@
 import { AiOutlinePhone } from "react-icons/ai";
 import { Main } from "./About.styled";
 import { Span_center } from "./Contacts.styled";
+import DG from "2gis-maps";
+import { useEffect } from "react";
 
 const Contacts = () => {
     return (
@@ -14,7 +16,7 @@ const Contacts = () => {
                 Бухгалтерия <AiOutlinePhone /> 8(7213) 98 36 58
             </Span_center>
             <Span_center>
-                Электрики <AiOutlinePhone /> 8(7213) 98 29 98; +7(708) 133 5335 
+                Электрики <AiOutlinePhone /> 8(7213) 98 29 98; +7(708) 133 5225
             </Span_center>
             <br />
             <Span_center>
@@ -28,28 +30,74 @@ const Contacts = () => {
             </Span_center>
 
             <div className="map_2gis">
-                <iframe
+                <Map />
+                {/* <iframe
                     frameborder="no"
                     title="2gis"
                     style={{ border: 0 }}
                     width="840"
                     height="500"
                     src="http://widgets.2gis.com/widget?type=firmsonmap&amp;options=%7B%22pos%22%3A%7B%22lat%22%3A50.064261%2C%22lon%22%3A73.008782%2C%22zoom%22%3A16%7D%2C%22opt%22%3A%7B%22city%22%3A%22karaganda%22%7D%2C%22org%22%3A%2270000001054917273%22%7D"
-                ></iframe>
+                ></iframe> */}
             </div>
 
             <div className="map_2gis_mobile">
-                <iframe
+                {/* <Map_mobile /> */}
+                {/* <iframe
                     frameborder="no"
                     title="2gis_mobile"
                     style={{ border: 0 }}
                     width="340"
                     height="400"
                     src="http://widgets.2gis.com/widget?type=firmsonmap&amp;options=%7B%22pos%22%3A%7B%22lat%22%3A50.064261%2C%22lon%22%3A73.008782%2C%22zoom%22%3A16%7D%2C%22opt%22%3A%7B%22city%22%3A%22karaganda%22%7D%2C%22org%22%3A%2270000001054917273%22%7D"
-                ></iframe>
+                ></iframe> */}
             </div>
         </Main>
     );
 };
+
+const Map = () => {
+    useEffect(() => {
+        let map;
+        map = DG.map("map-container", {
+            center: [50.064261, 73.008782],
+            zoom: 16,
+        });
+        DG.marker([50.064261, 73.008782])
+            .addTo(map)
+            .bindPopup(
+                "ТОО «Теміртау Қуат» пн-пт с 8.00 до 17.00, обед с 12.00 до 13.00"
+            );
+    }, []);
+
+    return (
+        <div
+            id="map-container"
+            style={{ width: "900px", height: "600px" }}
+        ></div>
+    );
+};
+
+// export const Map_mobile = () => {
+//     useEffect(() => {
+//         let map_mobile;
+//         map_mobile = DG.map("map-container", {
+//             center: [50.064261, 73.008782],
+//             zoom: 16,
+//         });
+//         DG.marker([50.064261, 73.008782])
+//             .addTo(map_mobile)
+//             .bindPopup(
+//                 "ТОО «Теміртау Қуат» пн-пт с 8.00 до 17.00, обед с 12.00 до 13.00"
+//             );
+//     }, []);
+
+//     return (
+//         <div
+//             id="map-container"
+//             style={{ width: "340px", height: "300px" }}
+//         ></div>
+//     );
+// };
 
 export default Contacts;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,9 @@ import { CiCircleChevRight } from "react-icons/ci";
 import LEP from "../img/lep.jpg";
 import "./Home.css";
 import { Main } from "./About.styled";
-import { Info, Info_p, P_icon } from "./Home.styled";
+import { Info, Info_p } from "./Home.styled";
+import { useEffect } from "react";
+import DG from "2gis-maps";
 
 const Home = () => {
     return (
@@ -16,10 +18,11 @@ const Home = () => {
                         <BiTime /> понедельник-пятница: 8:00 — 17:00
                     </p>
                     <p className="span_center">
-                        <AiOutlinePhone /> 8(7213) 98 36 58 - бухгалтерия
+                        <AiOutlinePhone /> <span className="phone_call"><a href="tel:87213983658">8(7213) 98 36 58</a></span> - бухгалтерия
                     </p>
-                    <p className="span_center">
-                        <AiOutlinePhone /> 8(7213) 98 29 98; +7(708) 133 5335  - дежурные электрики
+                    <p className="span_center_electro">
+                        <AiOutlinePhone /> <span className="phone_call"><a href="tel:87213982998">8(7213) 98 29 98;</a></span> <AiOutlinePhone /> <span className="phone_call"><a href="tel:+77081335225">+7(708) 133 5225</a></span>   -
+                        дежурные электрики
                     </p>
                     <p className="span_center">
                         <AiOutlineMail /> too.temirtaukuat@yandex.kz |
@@ -39,7 +42,8 @@ const Home = () => {
                         <CiCircleChevRight /> Передача электрической энергии
                     </p>
                     <p className="span_center">
-                        <CiCircleChevRight /> Подача воды по распределительным сетям
+                        <CiCircleChevRight /> Подача воды по распределительным
+                        сетям
                     </p>
                     <p className="span_center">
                         <CiCircleChevRight /> Отвод сточных вод
@@ -49,18 +53,44 @@ const Home = () => {
 
             <span className="location_on_phone">Где мы находимся:</span>
 
-            <div className="map_2gis_mobile">
-                <iframe
+            <div className="map_2gis_mobile" id="map-container" style={{ width: "333px", height: "300px" }}>
+                <Map_mobile style={{ width: "333px", height: "300px" }}
+				center={[50.064261, 73.008782]}
+				zoom={16}
+				/>
+                {/* <iframe
                     frameborder="no"
                     title="2gis_mobile"
                     style={{ border: 0 }}
                     width="340"
                     height="400"
                     src="http://widgets.2gis.com/widget?type=firmsonmap&amp;options=%7B%22pos%22%3A%7B%22lat%22%3A50.064261%2C%22lon%22%3A73.008782%2C%22zoom%22%3A16%7D%2C%22opt%22%3A%7B%22city%22%3A%22karaganda%22%7D%2C%22org%22%3A%2270000001054917273%22%7D"
-                ></iframe>
+                ></iframe> */}
             </div>
         </Main>
     );
+};
+
+const Map_mobile = () => {
+    useEffect(() => {
+        let map_mobile;
+        map_mobile = DG.map("map-container", {
+            center: [50.064261, 73.008782],
+            zoom: 16,
+        });
+        DG.marker([50.064261, 73.008782])
+            .addTo(map_mobile)
+            .bindPopup(
+                "ТОО «Теміртау Қуат» пн-пт с 8.00 до 17.00, обед с 12.00 до 13.00"
+            );
+    }, []);
+
+    // return (
+    //     <div
+    //         id="map-container"
+    //         style={{ width: "333px", height: "300px" }}
+    //     ></div>
+    // );
 };
 
 export default Home;
